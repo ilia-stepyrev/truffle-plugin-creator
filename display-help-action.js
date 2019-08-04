65791695fb7efda13cbb281793751b89bb9e7be2
ilia.stepyrev <ilya.stepyrev@valantmed.com>
2019-08-04 12:31:28 -0700

    update package and help

--- display-help-action.js
+++ display-help-action.js
@@ -1,3 +1,3 @@
 module.exports = () => {
-    console.log('Usage: truffle run creator <Contract1> <Contract2> <ContractN> --test <sol|js|none> --migration <new|current|skip>');
+    console.log('Usage: truffle run creator <Contract1> <Contract2> <ContractN> --test <sol|js|both|none> --migration <new|current|skip> --constr <true> --sol-min <version> --sol-max <version>');
 }
