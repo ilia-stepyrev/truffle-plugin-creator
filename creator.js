8dccb6d7a1f11be0c80ff63b1619f41367b2c8f5
ilia.stepyrev <ilya.stepyrev@valantmed.com>
2019-08-04 10:11:15 -0700

    blanks for generators and implement output module

--- creator.js
+++ creator.js
@@ -1,5 +1,12 @@
 const displayHelpAction = require('./display-help-action');
 const optionsParser = require('./options-parser');
+const outputModule = require('./output-module');
+
+const generators = [
+  require('./generators/contract-generator'),
+  require('./generators/test-generator'),
+  require('./generators/migration-generator'),
+]
 
 module.exports = async (config) => {
     if (config.help) {
@@ -9,5 +16,8 @@ module.exports = async (config) => {
 
     const options = optionsParser(config);
 
-    console.log(JSON.stringify(options));
+    options.contracts.forEach(contract => {
+      outputModule.logCaption(`Create: ${contract}`, 1, true);
+      generators.forEach(generator => generator(contract, options.args));
+    });
   }
---
+++ output-module.js
@@ -0,0 +1,26 @@
+class OutputModule {
+    constructor() {
+        this.intend = 0;
+    }
+
+    log(message, intend) {
+        console.log(this._getText(message, intend));
+    }
+
+    logCaption(caption, level, includeDashes) {
+        this.intend = (level - 1) * 2;
+        this.log(caption);
+        
+        if (includeDashes) {
+            this.log('-'.repeat(caption.length));
+        }
+
+        this.intend += 2;
+    }
+
+    _getText(message, extraIntend) {
+        return ' '.repeat(this.intend + extraIntend) + message;
+    }
+};
+
+module.exports = new OutputModule();
